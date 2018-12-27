42ccc7c42659cb282482c66fbcc24e853eed5f38
Pittawat2542 <pittawat2542@hotmail.com>
2018-12-27 10:16:51 +0700

    [ADD] template for / and /register

--- server/server.js
+++ server/server.js
@@ -11,16 +11,22 @@ const { knex } = require("./db/knex");
 const app = express();
 const publicPath = path.join(__dirname + "/../public");
 const viewPath = path.join(publicPath + "/views");
+const partialsPath = path.join(viewPath + "/partials");
 
 // Middlewares
 
+hbs.registerPartials(partialsPath);
 app.set("view engine", "hbs");
 app.set("views", viewPath);
 app.use(express.static(publicPath));
 
 // Routes
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/register", (req, res) => res.render("register"));
+
+app.post("/register", (req, res) => res.send("OK"))
 
 app.listen(process.env.PORT, () =>
   console.log(`Server is up on port ${process.env.PORT}`)
