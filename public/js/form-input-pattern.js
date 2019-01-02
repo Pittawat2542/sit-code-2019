e9d8ee50fa31287cfdf3bcd8422d89c53e9f1fdb
Pittawat2542 <pittawat2542@hotmail.com>
2019-01-02 16:45:34 +0700

    [UPDATE] /register add form and validation rules

---
+++ public/js/form-input-pattern.js
@@ -0,0 +1,20 @@
+new Cleave('[name="team_phone"]', {
+    numericOnly: true,
+    delimiters: ['-', '-', '-'],
+    blocks: [3, 4, 3]
+});
+new Cleave('[name="first_phone"]', {
+    numericOnly: true,
+    delimiters: ['-', '-', '-'],
+    blocks: [3, 4, 3]
+});
+new Cleave('[name="second_phone"]', {
+    numericOnly: true,
+    delimiters: ['-', '-', '-'],
+    blocks: [3, 4, 3]
+});
+new Cleave('[name="third_phone"]', {
+    numericOnly: true,
+    delimiters: ['-', '-', '-'],
+    blocks: [3, 4, 3]
+});
