1d615798be946778e36975718b7bb3c9b209ffaf
Pittawat2542 <pittawat2542@hotmail.com>
2019-01-02 21:13:30 +0700

    [UPDATE] template for /

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,3 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 13.652804, lng: 100.493642 },
-    zoom: 17
-  });
-
-  var infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-
-  service.getDetails({
-    placeId: 'ChIJ9ZZzpVGi4jARI56-Js0p2C8'
-  }, function (place, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
-      google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-          place.formatted_address + '</div>');
-        infowindow.open(map, this);
-      });
-    }
-  });
-}
-
 $(document).ready(function() {
   // fix menu when passed
   $(".masthead").visibility({
