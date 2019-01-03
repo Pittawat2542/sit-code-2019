38d06e887aea517c194fa79af3b26ef6b70de1f7
Pittawat2542 <pittawat2542@hotmail.com>
2019-01-03 21:40:25 +0700

    [UPDATE] footer and refactor css and js

--- public/js/script-index.js
+++ public/js/script-index.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  // fix menu when passed
   $(".masthead").visibility({
     once: false,
     onBottomPassed: function() {
@@ -9,7 +8,5 @@ $(document).ready(function() {
       $(".fixed.menu").transition("fade out");
     }
   });
-
-  // create sidebar and attach to menu open
   $(".ui.sidebar").sidebar("attach events", ".toc.item");
 });
